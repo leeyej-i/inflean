e8edaa41304a1e69d4379e4e0e164092a6d0119d
leeyej-i <leeyj111699@gmail.com>
2023-03-14 22:17:14 +0900

    setting

--- back/config/config.js
+++ back/config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   "development": {
     "username": "root",
-    "password": process.env.MYSQL_PASSWORD,
+    "password": "database",
     "database": "sleact",
     "host": "127.0.0.1",
     "dialect": "mysql"
