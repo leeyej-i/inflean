32b4afa79e0f37f1ef06f4c73129a5e8aa87c800
leeyej-i <leeyj111699@gmail.com>
2023-03-15 01:53:52 +0900

    sleact setting2

---
+++ sleact/alecture/layouts/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Router, Switch } from "react-router";
+
+const App = () => {
+    return (
+        <Switch>
+
+        </Switch>
+    )
+};
+
+export default App;
